c1a935a0db04329a65917c08831e3fc0416b4e39
Riyaz2815-Mohammed <anvarbhashaj1ace@gmail.com>
2026-07-12 23:23:33 +0530

    initial(commit)

---
+++ src/main/java/com/riyaz/banficotrainingprogram/BanficoTrainingProgramApplication.java
@@ -0,0 +1,13 @@
+package com.riyaz.banficotrainingprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanficoTrainingProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanficoTrainingProgramApplication.class, args);
+    }
+
+}
