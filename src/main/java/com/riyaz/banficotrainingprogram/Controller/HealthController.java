2c4693facd47538da2e287c426f43ede3550ca95
Riyaz2815-Mohammed <anvarbhashaj1ace@gmail.com>
2026-07-14 11:06:11 +0530

    feat: Introduced new endpoints just to check post method

--- src/main/java/com/riyaz/banficotrainingprogram/Controller/HealthController.java
+++ src/main/java/com/riyaz/banficotrainingprogram/Controller/HealthController.java
@@ -1,16 +1,28 @@
 package com.riyaz.banficotrainingprogram.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController()
+@RestController
 @RequestMapping("/api")
 public class HealthController {
     @GetMapping("/health")
-    public String gethealth(){
+    public String gethealth() {
         return "UP";
     }
 
+
+    @GetMapping("/love")
+    public String love() {
+        return "LOVE";
+    }
+    @GetMapping("/")
+    public String index() {
+        return "hello world";
     }
 
+    @PostMapping("/love")
+    public String post(@RequestBody String body) {
+        return "LOVE"+ " " + body;
+
+    }
+}
