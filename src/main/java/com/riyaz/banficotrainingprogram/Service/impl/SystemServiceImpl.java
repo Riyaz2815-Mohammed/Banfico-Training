0075ac21b7f50db05ccf5247924933fe864f3167
Riyaz2815-Mohammed <anvarbhashaj1ace@gmail.com>
2026-07-20 00:01:50 +0530

    fix: Used Constructor instead of setters and Found that gitproperties does not have message , so removed that

--- src/main/java/com/riyaz/banficotrainingprogram/Service/impl/SystemServiceImpl.java
+++ src/main/java/com/riyaz/banficotrainingprogram/Service/impl/SystemServiceImpl.java
@@ -3,15 +3,35 @@ package com.riyaz.banficotrainingprogram.Service.impl;
 import com.riyaz.banficotrainingprogram.Service.SystemService;
 import com.riyaz.banficotrainingprogram.dto.Healthresponse;
 import com.riyaz.banficotrainingprogram.dto.InfoResponse;
+import com.riyaz.banficotrainingprogram.metadata.GitInfoProvider;
+
+import java.time.LocalDateTime;
 
 public class SystemServiceImpl implements SystemService {
+    private final GitInfoProvider gitInfoProvider;
+
+    public SystemServiceImpl(GitInfoProvider gitInfoProvider) {
+        this.gitInfoProvider = gitInfoProvider;
+    }
+
     @Override
     public Healthresponse getHealth() {
-        return null;
+        Healthresponse response = new Healthresponse("UP", LocalDateTime.now());
+        return response;
     }
 
     @Override
     public InfoResponse getInfo() {
-        return null;
+        InfoResponse response = new InfoResponse("Banfico",gitInfoProvider.getCommitId(),gitInfoProvider.getBranch(),"v1",gitInfoProvider.getC
+        return new InfoResponse(
+                "Mini Banking System",
+                "1.0.0",
+                gitInfoProvider.getBranch(),
+                gitInfoProvider.getCommitId(),
+                gitInfoProvider.get(),
+                gitInfoProvider.getCommitMessage(),
+                gitInfoProvider.getBuildTime()
+        );
+        return ;
     }
 }
--- src/main/java/com/riyaz/banficotrainingprogram/dto/InfoResponse.java
+++ src/main/java/com/riyaz/banficotrainingprogram/dto/InfoResponse.java
@@ -6,62 +6,72 @@ public class InfoResponse {
     private String branch;
     private String commitId;
     private String commitTime;
-    private String commitMessage;
-    private String buildTime;
+//    private String commitMessage;
+//    private String buildTime;
+
+    public InfoResponse(String application, String commitId, String branch, String version, String commitTime) {
+        this.application = application;
+//        this.buildTime = buildTime;
+        this.commitId = commitId;
+        this.branch = branch;
+        this.version = version;
+//        this.commitMessage = commitMessage;
+        this.commitTime = commitTime;
+    }
 
     public String getApplication() {
         return application;
     }
 
-    public void setApplication(String application) {
-        this.application = application;
-    }
+//    public void setApplication(String application) {
+//        this.application = application;
+//    }
 
     public String getVersion() {
         return version;
     }
 
-    public void setVersion(String version) {
-        this.version = version;
-    }
+//    public void setVersion(String version) {
+//        this.version = version;
+//    }
 
     public String getBranch() {
         return branch;
     }
 
-    public void setBranch(String branch) {
-        this.branch = branch;
-    }
+//    public void setBranch(String branch) {
+//        this.branch = branch;
+//    }
 
     public String getCommitId() {
         return commitId;
     }
 
-    public void setCommitId(String commitId) {
-        this.commitId = commitId;
-    }
+//    public void setCommitId(String commitId) {
+//        this.commitId = commitId;
+//    }
 
     public String getCommitTime() {
         return commitTime;
     }
 
-    public void setCommitTime(String commitTime) {
-        this.commitTime = commitTime;
-    }
+//    public void setCommitTime(String commitTime) {
+//        this.commitTime = commitTime;
+//    }
 
-    public String getCommitMessage() {
-        return commitMessage;
-    }
+//    public String getCommitMessage() {
+//        return commitMessage;
+//    }
 
-    public void setCommitMessage(String commitMessage) {
-        this.commitMessage = commitMessage;
-    }
+//    public void setCommitMessage(String commitMessage) {
+//        this.commitMessage = commitMessage;
+//    }
 
-    public String getBuildTime() {
-        return buildTime;
-    }
-
-    public void setBuildTime(String buildTime) {
-        this.buildTime = buildTime;
-    }
+//    public String getBuildTime() {
+//        return buildTime;
+//    }
+//
+//    public void setBuildTime(String buildTime) {
+//        this.buildTime = buildTime;
+//    }
 }
