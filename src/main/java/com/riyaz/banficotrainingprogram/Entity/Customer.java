c7040ddc7c45db2cdc21e8bec599a223fe1d5dce
Riyaz2815-Mohammed <anvarbhashaj1ace@gmail.com>
2026-07-29 14:53:12 +0530

    fix : changed the email , pan to unique

--- src/main/java/com/riyaz/banficotrainingprogram/Entity/Customer.java
+++ src/main/java/com/riyaz/banficotrainingprogram/Entity/Customer.java
@@ -1,20 +1,18 @@
 package com.riyaz.banficotrainingprogram.Entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-
+@Entity
+@Table(name = "customers")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "customer_id", updatable = false, nullable = false)
     private UUID id;
 
-    @Column(name = "pan", nullable = false, length = 30)
+    @Column(name = "pan", nullable = false,unique = true, length = 10)
     private String pan;
 
     @Column(name = "first_name", nullable = false, length = 20)
@@ -23,17 +21,64 @@ public class Customer {
     @Column(name = "last_name", nullable = false, length = 20)
     private String lastName;
 
-    @Column(name = "email", nullable = false, length = 50)
+    @Column(name = "email", nullable = false,unique = true, length = 50)
     private String email;
 
     @Column(name = "phone_number", nullable = false, length = 20)
     private String phoneNumber;
 
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
 
-    @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;
+    protected Customer() {}
 
+    public Customer(String pan, String firstName, String lastName, String email, String phoneNumber, LocalDateTime createdAt) {
+        this.pan = pan;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public String getPan() {
+        return pan;
+    }
+
+    public void setPan(String pan) {
+        this.pan = pan;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
