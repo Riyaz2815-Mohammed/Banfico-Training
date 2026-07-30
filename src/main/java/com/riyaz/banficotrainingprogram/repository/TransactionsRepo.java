297ac995da311482f4ebb6abac240b5c776c34e8
Riyaz2815-Mohammed <anvarbhashaj1ace@gmail.com>
2026-07-31 00:47:57 +0530

    fix: The Transactions Type is Corrected , before it imported from jakarta transaction and now it uses Entity

--- src/main/java/com/riyaz/banficotrainingprogram/repository/TransactionsRepo.java
+++ src/main/java/com/riyaz/banficotrainingprogram/repository/TransactionsRepo.java
@@ -1,11 +1,12 @@
 package com.riyaz.banficotrainingprogram.repository;
 
+import com.riyaz.banficotrainingprogram.Entity.Transactions;
 import jakarta.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface TransactionsRepo extends JpaRepository<Transaction, UUID> {
-    List<Transaction> findByAccountId(UUID accountId);
+public interface TransactionsRepo extends JpaRepository<Transactions, UUID> {
+    List<Transactions> findByAccountId(UUID accountId);
 }
