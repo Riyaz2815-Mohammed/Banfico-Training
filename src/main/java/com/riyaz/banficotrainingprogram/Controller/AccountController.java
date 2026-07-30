4a90e8bff92074ea0ae4f07c889894355d9a547f
Riyaz2815-Mohammed <anvarbhashaj1ace@gmail.com>
2026-07-30 16:39:16 +0530

    fix: renamed api/account to api/accounts

--- src/main/java/com/riyaz/banficotrainingprogram/Controller/AccountController.java
+++ src/main/java/com/riyaz/banficotrainingprogram/Controller/AccountController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/account")
+@RequestMapping("/api/accounts")
 public class AccountController {
     private final AccountService  accountService;
     public  AccountController(AccountService accountService) {
